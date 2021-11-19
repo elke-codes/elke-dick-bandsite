08f27e5dfeeee12b065e751db4d6f20116abae7a
Elke Dick <elke.codes@gmail.com>
2021-11-18 20:55:27 -0800

    generate shows page with js create functions for each sub section to be dynamically generated

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,7 +1,5 @@
 // You must create the list of concerts using JavaScript DOM manipulation / flexbox layout.
 
-// You must have an array in JavaScript with all of concerts data and render the concerts HTML dynamically using the array data. It’s up to your discretion to decide on properties you create to represent all of the individual concert data.
-
 // No template literals should be used. All dynamic HTML should be added to DOM via DOM Methods for individual elements. Avoid bulk assigning stringified HTML using innerHTML
 
 // Utilize your knowledge of JavaScript DOM Manipulation and built in functions to create all the content between the hero image and the footer, as well as create your own functions as necessary. There should be no need to have any shows content housed within your html file for this section.
@@ -10,3 +8,142 @@
 // The individual rows of the Shows table need to have a hover state applied to them when a cursor is hovering over the table row, as per style guide. This can be done by utilizing a pseudo class within your Sass.
 // Additionally, clicking on an individual row should make that row "selected" or "active", applying a modifier CSS class via JavaScript. Utilize your knowledge of both JavaScript and Sass to accomplish this.
 // Your styling will still be applied through your Sass files. Do not use the built in JavaScript DOM style method.
+
+// You must have an array in JavaScript with all of concerts data and render the concerts HTML dynamically using the array data. It’s up to your discretion to decide on properties you create to represent all of the individual concert data.
+// https://www.freecodecamp.org/news/how-to-format-dates-in-javascript/
+// https://css-tricks.com/working-with-javascript-media-queries/
+const showsList = [
+  {
+    date: new Date("Mon Sept 06 2021").toDateString(),
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+
+  {
+    date: new Date(2021 / 09 / 2021).toDateString(),
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
+  },
+  {
+    date: 2021 / 10 / 15,
+    venue: "View Lounge",
+    location: "San Francisco, CA",
+  },
+  {
+    date: 2021 / 11 / 06,
+    venue: "Hyatt Agency",
+    location: "San Francisco, CA",
+  },
+  {
+    date: 2021 / 11 / 26,
+    venue: "Moscow Center",
+    location: "San Francisco, CA",
+  },
+  {
+    date: 2020 / 12 / 15,
+    venue: "Press Club",
+    location: "San Francisco, CA",
+  },
+];
+
+// !!! DATE VENUE LOCATION repeated for each show in mobile and only in columns for tablet and desktop
+//if min width is 768 don't include then for each, but render them on top of the page
+
+
+
+//select shows-section in html
+const showsSection = document.querySelector(".shows");
+
+// for each show in the array run the createshows function and append the showArticle of the current show to the showsSection
+
+//create shows section heading
+const showsTitle = document.createElement('h2');
+showsTitle.classList.add(".shows__title");
+showsTitle.innerText = "Shows";
+showsSection.appendChild(showsTitle);
+
+for (let i = 0; i < showsList.length; i++) {
+  let currentShow = showsList[i];
+
+  let showArticle = createShows(currentShow);
+
+  showsSection.appendChild(showArticle);
+}
+
+//createShows function
+function createShows(currentShow) {
+
+  //create show article wrapper
+  const showArticle = document.createElement("article");
+  showArticle.classList.add(".show");
+  showsSection.appendChild(showArticle);
+
+  // create date 
+  const dateContainer = createDate(currentShow);
+  showArticle.appendChild(dateContainer);
+
+  //create venue 
+  const venueContainer = createVenue(currentShow);
+  showArticle.appendChild(venueContainer);
+
+  //create location
+  const locationContainer = createLocation(currentShow);
+  showArticle.appendChild(locationContainer);
+
+
+  return showArticle;
+
+}
+
+
+function createDate(currentShow) {
+
+  const dateContainer = document.createElement("div");
+
+  const dateTitle = document.createElement("h4")
+  dateTitle.classList.add(".show__title");
+  dateTitle.innerText = "DATE";
+  dateContainer.appendChild(dateTitle);
+
+  const showDate = document.createElement("p");
+  showDate.classList.add(".show__date");
+  showDate.innerText = currentShow.date;
+  dateContainer.appendChild(showDate);
+
+  return dateContainer;
+};
+
+
+function createVenue(currentShow) {
+  const venueContainer = document.createElement("div");
+
+  const venueTitle = document.createElement("h4");
+  venueTitle.classList.add("show__title");
+  venueTitle.innerText = "VENUE";
+  venueContainer.appendChild(venueTitle);
+
+  const showVenue = document.createElement("p");
+  showVenue.classList.add(".show__venue");
+  showVenue.innerText = currentShow.venue;
+  venueContainer.appendChild(showVenue);
+  ;
+  return venueContainer
+}
+
+
+function createLocation(currentShow) {
+  const locationContainer = document.createElement("div");
+
+
+  const locationTitle = document.createElement("h4");
+  locationTitle.classList.add("show__title");
+  locationTitle.innerText = "LOCATION";
+  locationContainer.appendChild(locationTitle);
+
+  const showLocation = document.createElement("p");
+  showLocation.classList.add(".show__location");
+  showLocation.innerText = currentShow.location;
+  locationContainer.appendChild(showLocation);
+
+  return locationContainer;
+}
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,50 +1,166 @@
 
-// create comments array, with objects for each comment
-//comments must have:
-// - a name - a timestamp - the comment text
-const comments = [
-  {
+// // create comments array, with objects for each comment
+// //comments must have:
 
-    name: "Connor Walton",
-    message: `This is art. This is inexplicable magic
-    expressed in the purest way, everything
-    that makes up this majestic work
-    deserves reverence. Let us appreciate
-    this for what it is and what it contains.`,
-    // date timestamp: 02/17/2021 as the right format, for new comment add current date in the right format
-    // img : url();
-  },
-  {
-    name: "Emilie Beach",
-    message: `I feel blessed to have seen them in
-    person. What a show! They were just
-    perfection. If there was one day of my
-    life I could relive, this would be it. What
-    an incredible day.`,
-     // date/timestamp: 01/09/2021 as the right format, for new comment add current date in the right format
-     // img : url();
-  },
-  {
-    name: "Miles Acosta",
-    message: `I can t stop listening. Every time I hear
-    one of their songs the vocals it gives
-    me goosebumps. Shivers straight down
-    my spine. What a beautiful expression of
-    creativity. Can t get enough.`,
-    // date/timestamp: 12/20/2020 as the right format, for new comment add current date in the right format
-    // img : url();
-  }
-]
+// // const { format } = require("path/posix");
 
-// You must have a function called displayComment() that takes in one comment object as a parameter and displays it on the page using JavaScript DOM manipulation.
+// // - a name - a timestamp - the comment text
+// const comments = [
+//   {
 
-// No template literals should be used. All dynamic HTML should be added to DOM via DOM Methods for individual elements. Avoid bulk assigning stringified HTML using innerHTML
+//     name: "Connor Walton",
+//     message: `This is art. This is inexplicable magic
+//     expressed in the purest way, everything
+//     that makes up this majestic work
+//     deserves reverence. Let us appreciate
+//     this for what it is and what it contains.`,
+//     // date timestamp: 02/17/2021 as the right format, for new comment add current date in the right format
+//     // img : url();
+//   },
+//   {
+//     name: "Emilie Beach",
+//     message: `I feel blessed to have seen them in
+//     person. What a show! They were just
+//     perfection. If there was one day of my
+//     life I could relive, this would be it. What
+//     an incredible day.`,
+//      // date/timestamp: 01/09/2021 as the right format, for new comment add current date in the right format
+//      // img : url();
+//   },
+//   {
+//     name: "Miles Acosta",
+//     message: `I can t stop listening. Every time I hear
+//     one of their songs the vocals it gives
+//     me goosebumps. Shivers straight down
+//     my spine. What a beautiful expression of
+//     creativity. Can t get enough.`,
+//     // date/timestamp: 12/20/2020 as the right format, for new comment add current date in the right format
+//     // img : url();
+//   }
+// ]
 
-// You must use an HTML Form with the following functionality:
-// That submits using the addEventListener
-// Prevents the page from reloading when submitting a new comment
-// Constructs a new comment object
-// Pushes a new comment object to an array of comments
-// Clears all comments from the page
-// Re-renders to the page all comments from the comment array
-// Clears the input fields after submitting a new comment
+// /// --   COMMENTS SECTION
+
+// // There must be a button that allows a user to add a new comment.
+// // The user must be able to add their name and a comment.
+// // The comments must be added such that the newest comments are at the top.
+// // 3 Default comments must be displayed when the page first loads.
+
+
+
+// // No template literals should be used. All dynamic HTML should be added to DOM via DOM Methods for individual elements. Avoid bulk assigning stringified HTML using innerHTML
+
+
+
+
+
+
+
+
+
+// // You must have a function called displayComment() that takes in one comment object as a parameter and displays it on the page using JavaScript DOM manipulation.
+
+// // function displayComment (comment){
+//    // Constructs a new comment object
+//   // a name comment date img
+// const commentsOld = document.querySelector(".comments__old");
+
+// forEach()
+
+// function createComment (comments){
+
+// const article = document.createElement("article");
+// article.classList.add("comment");
+
+// //function createAvatar
+
+// const commentsName = document.createElement("p");
+// commentsName.classList.add("comments__name");
+// commentsName.innterText = comments.name; /*comments[i].name;*/
+
+// const commentsDate = document.createElement("p");
+// commentsDate.classList.add("comments__date");
+// commentsDate.innerText = comments.date; /*comments[i].name;*/
+
+// const comment = document.createElement("p");
+// comment.classList.add("comments__comment");
+// comment.innerText = comments.comment /*comments[i].name;*/
+
+// // const commentsNew = document.querySelector(".comments_new");
+
+// commentsNew.appendChild(article);
+// article.appendChild(avatar);
+// avatar.appendChild(avatarPlaceHolder);
+// avatar.appendChild(avatarImage);
+// article.appendChild(commentsName);
+// article.appendChild(commentsDate);
+// article.appendChild(comment);
+
+
+
+// }
+
+// createComment();
+
+// function createAvatar (comments){
+//   console.log(comments);
+//   const avatar = document.createElement("div");
+// avatar.classList.add("avatar");
+// const avatarPlaceHolder = document.createElement("div");
+// avatarPlaceHolder.classList.add("avatar__placeholder");
+// const avatarImage = document.createElement("img");
+// avatarImage.classList.add("avatar__image");
+// avatarImage.src = "#";
+// avatarImage.alt = "profile picture";
+// }
+
+
+// // You must use an HTML Form with the following functionality:
+
+
+
+// // Pushes a new comment object to an array of comments
+// // Clears all comments from the page
+// // Re-renders to the page all comments from the comment array
+
+
+// // insert newHeading before FirstHeading
+// // parent.insertBefore(newHeading, firstHeading);
+
+
+// const commentsForm = document.querySelector("comments__form");
+
+// // That submits using the addEventListener
+// commentsForm.addEventlistener("submit", handleFormSubmission);
+
+
+// function handleFormSubmission(e){
+//   // Prevents the page from reloading when submitting a new comment
+//   e.preventDefault();
+
+
+//   // Clears the input fields after submitting a new comment
+//   e.target.reset();
+// }
+
+
+
+
+
+
+// ///-- DIVING DEEPER
+
+// // Dynamic Timestamp
+// // To add a more realistic feel to the site, try updating the timestamp in the comments section to reflect when it was posted in a more human-readable format. Using YouTube as an example, a recently posted comment might display the time posted as "10 minutes ago" or "3 days ago". Apply this type of timestamp to your data without hardcoding the actual message.
+
+// // Form Validation
+// // In order to increase the effectiveness of the comments form, implement validation to check for empty form fields.
+
+// // Upon validation, if there are any errors:
+
+// // Prevent the form from submitting.
+// // Adjust the state of the form fields to show the error state that is specified in the Style Guide.
+// // If there are no errors:
+
+// // Clear any error states from the form fields.
+// // Submit the form.
